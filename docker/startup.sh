b254fe0f9080e420637c2c81c40c5f16c250594c
Casey Doerschuk <casey.doerschuk@teamaol.com>
2016-07-15 17:30:01 -0400

    stuff added for dynamic setting of cache hosts

---
+++ docker/startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sed -ie 's/---REDIS_HOSTS---/'$REDIS_HOSTS'/g' $WORKDIR/resources/splicer.conf
+echo "------ config -------"
+cat $WORKDIR/resources/splicer.conf | grep caching.hosts
+echo "------ config end-------"
+java -server -classpath $WORKDIR/resources:$WORKDIR:$WORKDIR/tsdb-splicer-all-$VERSION.jar com.turn.splicer.SplicerMain --port $SPLICER_PORT --config=$WORKDIR/resources/splicer.conf
